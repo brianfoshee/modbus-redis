4a11bad5b3ec1ab09120074068441197b0d028c6
Brian Foshee <brianfoshee@gmail.com>
2013-10-24 20:09:33 -0400

    Refactor: split into jobs to split into threads
    Threads not working right now but the loop is
    split up to prepare for it. Also writing to file.

--- send.c
+++ send.c
@@ -21,66 +21,150 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
+#include <pthread.h>
 #include <curl/curl.h>
 
 #include <json-c/json.h>
 #include <hiredis/hiredis.h>
 
-void handle_reply(redisReply *reply);
+#define NUMTHREADS 8
+
+struct ThreadData {
+    int start, stop;
+    struct redisReply **array;
+};
+
+redisContext *c;
+redisReply *keysReply, *tstampReply;
+json_object *baseobj;
+
+void* processKeys(void *td);
+void processKey(char *);
 redisContext* redis_conn(void);
 void redis_disconn(redisContext *c);
+void handle_reply(redisReply *reply);
 
 int main(int argc, char ** argv) {
-  int i = 0;
-  char *tstamp, *val, *key;
-  redisContext *c;
-  json_object *baseobj, *powerobj, *r_dbl;
-  redisReply *tstamp_reply, *keysReply, *tmpReply;
+  int numKeys, i, start, stop, jobsPerThread;
+  struct ThreadData data[NUMTHREADS];
+  pthread_t thread[NUMTHREADS];
 
   c = redis_conn();
-  baseobj = json_object_new_object();
-  tstamp_reply = redisCommand(c, "smembers timestamps");
-  keysReply = redisCommand(c, "keys *");
-
-  // Initialize curl
   curl_global_init(CURL_GLOBAL_SSL);
+  baseobj = json_object_new_object();
+  tstampReply = redisCommand(c, "smembers timestamps");
+  keysReply = redisCommand(c, "keys *");
+  numKeys = (int) keysReply->elements;
+  jobsPerThread = (numKeys + NUMTHREADS - 1) / NUMTHREADS;
 
-  for (i = 0; i < keysReply->elements; i++) {
-    // ignore key called 'timestamps'
-    key = keysReply->element[i]->str;
+  fprintf(stdout, "Number of keys %d\nJobs per thread %d\n", numKeys, jobsPerThread);
 
-    if (strcmp(key, "timestamps") != 0) {
-      // get all key/vals for this string and store them in a json obj
-      powerobj = json_object_new_object();
-      for (int j = 0; j < tstamp_reply->elements; j++) {
-        // timestamp
-        tstamp = tstamp_reply->element[j]->str;
-        // get the value for this timestamp
-        tmpReply = redisCommand(c, "HGET %s %s", key, tstamp);
-        // add key:val pair to powerobj
-        val = tmpReply->str;
-        if (val != NULL) {
-          r_dbl = json_object_new_double(atof(val));
-          json_object_object_add(powerobj, tstamp, r_dbl);
-        }
-        freeReplyObject(tmpReply);
-      }
-      // add the json obj for this key to the base obj
-      json_object_object_add(baseobj, key, powerobj);
-    }
+  for (i = 0; i < NUMTHREADS; i++) {
+    data[i].start = i * jobsPerThread;
+    data[i].stop = (i + 1) * jobsPerThread;
+    data[i].array = keysReply->element;
 
+    if (data[i].stop > numKeys)
+      data[i].stop = numKeys;
   }
 
-  freeReplyObject(tstamp_reply);
-  freeReplyObject(keysReply);
+  // Go through each group and send to function for each key
+  for (i = 0; i < NUMTHREADS; i++) {
+    // pthread_create(&thread[i], NULL, processKeys, &data[i]);
+    processKeys(&data[i]);
+  }
+
+  for (i = 0; i < NUMTHREADS; i++) {
+    pthread_join(thread[i], NULL);
+  }
 
   redis_disconn(c);
-
   curl_global_cleanup();
+  freeReplyObject(tstampReply);
+  freeReplyObject(keysReply);
 
-  printf("%s\n", json_object_to_json_string(baseobj));
+  FILE *f = fopen("/Users/brian/Desktop/out.json", "w");
+  if (f == NULL)
+  {
+      printf("Error opening file!\n");
+      exit(1);
+  }
+
+  fprintf(f, "%s\n", json_object_to_json_string(baseobj));
+
+  fclose(f);
+
+  // fprintf(stdout, "%s\n", json_object_to_json_string(baseobj));
 
   return 0;
+}
+
+void* processKeys(void *td) {
+  struct ThreadData* data = td;
+  int start = data->start;
+  int stop = data->stop;
+  redisReply **array = data->array;
+  int i;
+  char *key;
+
+  fprintf(stdout, "Processing %d to %d\n", start, stop);
+
+  for (int i = start; i < stop; i++) {
+    key = array[i]->str;
+    if (strcmp(key, "timestamps") != 0)
+      processKey(key);
+  }
+
+  return NULL;
+}
+
+void processKey(char *key) {
+  fprintf(stdout, "Processing key %s\n", key);
+
+  json_object *powerobj, *r_dbl;
+  redisReply *tmpReply;
+  char *tstamp, *val;
+
+  powerobj = json_object_new_object();
+
+  for (int j = 0; j < tstampReply->elements; j++) {
+    // timestamp
+    tstamp = tstampReply->element[j]->str;
+    // get the value for this timestamp
+    tmpReply = redisCommand(c, "HGET %s %s", key, tstamp);
+    // add key:val pair to powerobj
+    val = tmpReply->str;
+    if (val != NULL) {
+      r_dbl = json_object_new_double(atof(val));
+      json_object_object_add(powerobj, tstamp, r_dbl);
+    }
+    freeReplyObject(tmpReply);
+  }
+  // add the json obj for this key to the base obj
+  json_object_object_add(baseobj, key, powerobj);
+}
+
+redisContext* redis_conn(void) {
+  redisContext *c;
+  const char *hostname = "127.0.0.1";
+  int port = 6380;
+
+  struct timeval timeout = { 1, 500000 };
+  c = redisConnectWithTimeout(hostname, port, timeout);
+  if (c == NULL || c->err) {
+    if (c) {
+      printf("Connection error: %s\n", c->errstr);
+      redisFree(c);
+    } else {
+      printf("Connection error: can't allocate redis context\n");
+    }
+    exit(-1);
+  }
+  return c;
+}
+
+void redis_disconn(redisContext *c) {
+  redisFree(c);
 }
 
 void handle_reply(redisReply *reply) {
@@ -117,27 +201,4 @@ void handle_reply(redisReply *reply) {
       break;
     }
   }
-}
-
-redisContext* redis_conn(void) {
-  redisContext *c;
-  const char *hostname = "127.0.0.1";
-  int port = 6380;
-
-  struct timeval timeout = { 1, 500000 };
-  c = redisConnectWithTimeout(hostname, port, timeout);
-  if (c == NULL || c->err) {
-    if (c) {
-      printf("Connection error: %s\n", c->errstr);
-      redisFree(c);
-    } else {
-      printf("Connection error: can't allocate redis context\n");
-    }
-    exit(-1);
-  }
-  return c;
-}
-
-void redis_disconn(redisContext *c) {
-  redisFree(c);
 }
