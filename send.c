474a6f538fde825027b6c5e554b1972d9a880a12
Brian Foshee <brianfoshee@gmail.com>
2013-10-24 21:48:20 -0400

    ALL THE THREADS

--- send.c
+++ send.c
@@ -32,15 +32,12 @@
 struct ThreadData {
   int start, stop;
   size_t size;
+  json_object *baseObj;
   char **keys, **tstamps;
 };
 
-redisContext *c;
-redisReply *keysReply, *tstampReply;
-json_object *baseobj;
-
 void* processKeys(void *td);
-void processKey(char *, char **tstamps, size_t size);
+void processKey(char *key, char **tstamps, size_t, json_object *);
 redisContext* redis_conn(void);
 void redis_disconn(redisContext *c);
 void handle_reply(redisReply *reply);
@@ -48,12 +45,16 @@ void handle_reply(redisReply *reply);
 int main(int argc, char ** argv) {
   int numKeys, numTstamps, i, skip, start, stop, jobsPerThread;
   char **keys, **tstamps, *key;
+  redisReply *keysReply, *tstampReply;
   struct ThreadData data[NUMTHREADS];
   pthread_t thread[NUMTHREADS];
+  json_object *baseObj;
+  redisContext *c;
+  FILE *f;
 
   c = redis_conn();
   curl_global_init(CURL_GLOBAL_SSL);
-  baseobj = json_object_new_object();
+  baseObj = json_object_new_object();
   tstampReply = redisCommand(c, "smembers timestamps");
   keysReply = redisCommand(c, "keys *");
   numKeys = (int) keysReply->elements - 1;
@@ -102,6 +103,7 @@ int main(int argc, char ** argv) {
     data[i].size = numTstamps;
     data[i].keys = keys;
     data[i].tstamps = tstamps;
+    data[i].baseObj = baseObj;
 
     if (data[i].stop > numKeys)
       data[i].stop = numKeys;
@@ -109,14 +111,16 @@ int main(int argc, char ** argv) {
 
   for (i = 0; i < NUMTHREADS; i++)
   {
-    // pthread_create(&thread[i], NULL, processKeys, &data[i]);
-    processKeys(&data[i]);
+    pthread_create(&thread[i], NULL, processKeys, &data[i]);
+    // processKeys(&data[i]);
   }
 
   for (i = 0; i < NUMTHREADS; i++)
   {
     pthread_join(thread[i], NULL);
   }
+
+  fprintf(stdout, "waiting");
 
   for (i = 0; i < numKeys; i++)
   {
@@ -134,16 +138,19 @@ int main(int argc, char ** argv) {
   redis_disconn(c);
   curl_global_cleanup();
 
-  // FILE *f = fopen("/Users/brian/Desktop/out.json", "w");
-  // if (f == NULL)
-  // {
-  //     printf("Error opening file!\n");
-  //     exit(1);
-  // }
+  fprintf(stdout, "freed");
 
-  fprintf(stdout, "%s\n", json_object_to_json_string(baseobj));
+  f = fopen("/Users/brian/Desktop/out.json", "w");
 
-  // fclose(f);
+  if (f == NULL)
+  {
+      printf("Error opening file!\n");
+      exit(1);
+  }
+
+  fprintf(f, "%s\n", json_object_to_json_string(baseObj));
+
+  fclose(f);
 
   return 0;
 }
@@ -156,6 +163,7 @@ void* processKeys(void *td)
   size_t size = data->size;
   char **keys = data->keys;
   char **tstamps = data->tstamps;
+  json_object *baseObj = data->baseObj;
   int i;
   char *key;
 
@@ -163,23 +171,26 @@ void* processKeys(void *td)
 
   for (int i = start; i < stop; i++) {
     key = keys[i];
-    processKey(key, tstamps, size);
+    processKey(key, tstamps, size, baseObj);
   }
 
   return NULL;
 }
 
-void processKey(char *key, char **tstamps, size_t size)
+void processKey(char *key, char **tstamps, size_t size, json_object *baseObj)
 {
   json_object *powerobj, *r_dbl;
   redisReply *tmpReply;
   char *val, *tstamp;
+  redisContext *c;
 
+  fprintf(stdout, "Processing key %s\n", key);
+
+  c = redis_conn();
   powerobj = json_object_new_object();
 
   for (int j = 0; j < (int) size; j++) {
     tstamp = tstamps[j];
-    fprintf(stdout, "Processing key %s %s\n", key, tstamp);
     // get the value for this timestamp
     tmpReply = redisCommand(c, "HGET %s %s", key, tstamp);
     // add key:val pair to powerobj
@@ -191,7 +202,8 @@ void processKey(char *key, char **tstamps, size_t size)
     freeReplyObject(tmpReply);
   }
   // add the json obj for this key to the base obj
-  json_object_object_add(baseobj, key, powerobj);
+  json_object_object_add(baseObj, key, powerobj);
+  redis_disconn(c);
 }
 
 redisContext* redis_conn(void) {
